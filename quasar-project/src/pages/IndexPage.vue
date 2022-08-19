eda6315e4055ef79dade5d8197e6ee29c719fdd3
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-19 14:59:41 +0200

    rename Example layout to Ingredients

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,22 +1,22 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
+    <ingredients
       title="Example component"
       active
       :todos="todos"
       :meta="meta"
-    ></example-component>
+    ></ingredients>
   </q-page>
 </template>
 
 <script lang="ts">
 import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import Ingredients from 'components/Ingredients.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'IndexPage',
-  components: { ExampleComponent },
+  components: { Ingredients },
   setup () {
     const todos = ref<Todo[]>([
       {
