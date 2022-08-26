31543af257b65ff481cf71de8e88081e9da29e5d
Fabian Geyer <st148794@stud.uni-stuttgart.de>
2022-08-26 17:29:53 +0200

    rename and restructure vue files

---
+++ quasar-project/src/components/IngredientAdd.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <q-btn round color='accent' icon='add' />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IngredientAdd',
+};
+</script>
--- quasar-project/src/pages/Barkeeper.vue
+++ quasar-project/src/pages/Barkeeper.vue
@@ -1,17 +1,21 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <ingredients title="Ingredients"></ingredients>
+    <div class="items-center column justify-evenly">
+      <ingredient-add></ingredient-add>
+      <ingredient-table title="Ingredients"></ingredient-table>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import Ingredients from '../components/Ingredients.vue';
+import IngredientTable from '../components/IngredientTable.vue';
+import IngredientAdd from '../components/IngredientAdd.vue';
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'BarkeeperPage',
-  components: { Ingredients },
+  components: { IngredientTable, IngredientAdd },
   setup() {
     const router = useRouter();
     return { router };
